79bb5c38929023b7a91e71b226e7da35fedd45fc
DanielGruber <daniel@ibpg.eu>
2012-10-11 23:42:11 +0200

    Throws an Error if language or status not given

--- mysite/application/admin/settingsAdmin.php
+++ mysite/application/admin/settingsAdmin.php
@@ -4,7 +4,7 @@
   *@link http://goma-cms.org
   *@license: http://www.gnu.org/licenses/gpl-3.0.html see 'license.txt'
   *@Copyright (C) 2009 - 2012  Goma-Team
-  * last modified: 09.02.2012
+  * last modified: 11.10.2012
 */
 
 defined('IN_GOMA') OR die('<!-- restricted access -->'); // silence is golden ;)
@@ -29,8 +29,12 @@ class settingsAdmin extends adminItem
 		}
 		
 		public function submit_form($data, $form, $model = null) {
-			$status = (SITE_MODE == STATUS_DISABLED) ? STATUS_DISABLED : $data["status"]; 
-			writeProjectConfig(array('lang' => $data["lang"], "status" => $status, "timezone" => $data["timezone"], "date_format" => $data["date_format"]));
+			if(isset($data["lang"], $data["status"], $data["timezone"], $data["date_format"])) {
+				$status = (SITE_MODE == STATUS_DISABLED) ? STATUS_DISABLED : $data["status"]; 
+				writeProjectConfig(array('lang' => $data["lang"], "status" => $status, "timezone" => $data["timezone"], "date_format" => $data["date_format"]));
+			} else {
+				throwError(6, "Invalid-Error", "Too less keys in data to write settings.");
+			}
 			parent::safe($data, $form, $model);
 		}
 }
