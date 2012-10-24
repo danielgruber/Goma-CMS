ae83ee36c8336735d5f317c65b7c0d3e2c63f767
DanielGruber <daniel@ibpg.eu>
2012-10-24 23:36:02 +0200

    Update to 049
    KeyChain now used in Goma CMS

--- mysite/application/control/contentcontroller.php
+++ mysite/application/control/contentcontroller.php
@@ -5,8 +5,8 @@
   *@link http://goma-cms.org
   *@license: http://www.gnu.org/licenses/gpl-3.0.html see 'license.txt'
   *@Copyright (C) 2009 - 2012  Goma-Team
-  * last modified: 08.07.2012
-  * $Version 2.0.1
+  * last modified: 24.10.2012
+  * $Version 2.0.2
 */
 defined('IN_GOMA') OR die('<!-- restricted access -->'); // silence is golden ;)
 
@@ -80,11 +80,11 @@ class contentController extends FrontedController
 				$this->callExtending("providePasswords", $passwords);
 				if($this->modelInst()->read_permission->password != "" || $passwords) {
 					$password = $this->modelInst()->read_permission->password;
-					if((isset($_SESSION["keychain"]) && in_array($this->modelInst()->read_permission->password, $_SESSION["keychain"])) || (isset($_COOKIE["keychain_" . md5(md5($password))]) && $_COOKIE["keychain_" . md5(md5($password))] == md5($password))) {
+					if($this->KeyChainCheck($password)) {
 						;
 					} else {
 						foreach($passwords as $pwd) {
-							if((isset($_SESSION["keychain"]) && in_array($pwd, $_SESSION["keychain"])) ||  (isset($_COOKIE["keychain_" . md5(md5($pwd))]) && $_COOKIE["keychain_" . md5(md5($pwd))] == md5($pwd))) {
+							if($this->KeyChainCheck($pwd)) {
 								$found = true;
 							}
 						}
@@ -100,12 +100,7 @@ class contentController extends FrontedController
 							Core::settitle($this->modelInst()->title);*/
 							
 							if($pwd = $this->prompt(lang("password", "password"), array($validator), null, null, true)) {
-								setCookie("keychain_" . md5(md5($pwd)), md5($pwd), time() + 60 * 60 * 24 * 181, "/");
-								if(isset($_SESSION["keychain"])) {
-									$_SESSION["keychain"][] = $pwd;
-								} else {
-									$_SESSION["keychain"] = array($pwd);
-								}
+								$this->keyChainAdd($pwd);
 								//$hasAction = true;
 							} else {
 								return 0;
