7187d6d411f4195b08c66ab52c8f6f0b920a8590
DanielGruber <daniel@ibpg.eu>
2012-11-29 00:17:56 +0100

    Optimized Diff-Function with third-party-class

--- system/libs/history/historyController.php
+++ system/libs/history/historyController.php
@@ -173,8 +173,10 @@ class HistoryController extends Controller {
 						$newversiondata = convert::raw2text($newversiondata);
 					}
 					
-					$diff = $this->htmlDiff($oldversiondata, $newversiondata);
-					$fieldset->push(array("title" => $title, "content" => trim($diff)));
+					$object = new diff_match_patch;
+					$diff = $object->diff_compute($oldversiondata, $newversiondata, true);
+					$object->diff_cleanupEfficiency($diff);
+					$fieldset->push(array("title" => $title, "content" => trim($this->diffToHTML($diff))));
 				}
 			}
 			
@@ -215,54 +217,45 @@ class HistoryController extends Controller {
 		throwError(6, "Invalid-Data-Error", "$field doesn't exist on version of type ".$version->class." with id ".$version->versionid."");
 	}
 	
-	/*
-    Paul's Simple Diff Algorithm v 0.1
-    (C) Paul Butler 2007 <http://www.paulbutler.org/>
-    May be used and distributed under the zlib/libpng license.
-
-    This code is intended for learning purposes; it was written with short
-    code taking priority over performance. It could be used in a practical
-    application, but there are a few ways it could be optimized.
-
-    Given two arrays, the function diff will return an array of the changes.
-    I won't describe the format of the array, but it will be obvious
-    if you use print_r() on the result of a diff on some test data.
-
-    htmlDiff is a wrapper for the diff command, it takes two strings and
-    returns the differences in HTML. The tags used are <ins> and <del>,
-    which can easily be styled with CSS.  
+	/**
+	 * converts diff to HTML
+	 *
+	 *@name diffToHTML
 	*/
-	
-	function diff($old, $new){
-		$maxlen = 0;
-	    foreach($old as $oindex => $ovalue){
-	        $nkeys = array_keys($new, $ovalue);
-	        foreach($nkeys as $nindex){
-	            $matrix[$oindex][$nindex] = isset($matrix[$oindex - 1][$nindex - 1]) ?
-	                $matrix[$oindex - 1][$nindex - 1] + 1 : 1;
-	            if($matrix[$oindex][$nindex] > $maxlen){
-	                $maxlen = $matrix[$oindex][$nindex];
-	                $omax = $oindex + 1 - $maxlen;
-	                $nmax = $nindex + 1 - $maxlen;
-	            }
-	        }   
-	    }
-	    if($maxlen == 0) return array(array('d'=>$old, 'i'=>$new));
-	    return array_merge(
-	        $this->diff(array_slice($old, 0, $omax), array_slice($new, 0, $nmax)),
-	        array_slice($new, $nmax, $maxlen),
-	        $this->diff(array_slice($old, $omax + $maxlen), array_slice($new, $nmax + $maxlen)));
-	}
-	
-	function htmlDiff($old, $new){
-	    $diff = $this->diff(explode(' ', $old), explode(' ', $new));
-	    $ret = "";
-	    foreach($diff as $k){
-	        if(is_array($k))
-	            $ret .= (!empty($k['d'])?"<del>".implode(' ',$k['d'])."</del> ":'').
-	                (!empty($k['i'])?"<ins>".implode(' ',$k['i'])."</ins> ":'');
-	        else $ret .= $k . ' ';
-	    }
-	    return $ret;
+	public function diffToHTML($diffs) {
+		$html = array ();
+		$i = 0;
+		for ($x = 0; $x < count($diffs); $x++) {
+			$op = $diffs[$x][0]; // Operation (insert, delete, equal)
+			$data = $diffs[$x][1]; // Text of change.
+			/*$text = preg_replace(array (
+				'/&/',
+				'/</',
+				'/>/',
+				"/\n/"
+			), array (
+				'&amp;',
+				'&lt;',
+				'&gt;',
+				'&para;<BR>'
+			), $data);*/
+			$text = $data;
+
+			switch ($op) {
+				case DIFF_INSERT :
+					$html[$x] = '<INS STYLE="background:#E6FFE6;" TITLE="i=' . $i . '">' . $text . '</INS>';
+					break;
+				case DIFF_DELETE :
+					$html[$x] = '<DEL STYLE="background:#FFE6E6;" TITLE="i=' . $i . '">' . $text . '</DEL>';
+					break;
+				case DIFF_EQUAL :
+					$html[$x] = '<SPAN TITLE="i=' . $i . '">' . $text . '</SPAN>';
+					break;
+			}
+			if ($op !== DIFF_DELETE) {
+				$i += mb_strlen($data);
+			}
+		}
+		return implode('',$html);
 	}
 }
