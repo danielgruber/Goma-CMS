ef37d40b1990c4bdfe8a7c85d5c37872aed286f5
Daniel Gruber <igruber@me.com>
2012-12-04 14:23:56 +0100

    default.css less code, but new goma_default.css

---
+++ system/libs/notifications/notifications.php
@@ -0,0 +1,15 @@
+<?php
+/**
+  *@package goma framework
+  *@link http://goma-cms.org
+  *@license: http://www.gnu.org/licenses/gpl-3.0.html see 'license.txt'
+  *@Copyright (C) 2009 - 2012  Goma-Team
+  * last modified: 03.12.2012
+  * $Version 1.0.1
+*/
+
+defined('IN_GOMA') OR die('<!-- restricted access -->'); // silence is golden ;)
+
+class Notification extends DataObject {
+	
+}
