09803736d442ad3e50c133ab430eb11a09777ef1
Daniel Gruber <igruber@me.com>
2012-12-16 22:35:45 +0100

    Fixed some Version Bugs

--- system/core/ClassInfo.php
+++ system/core/ClassInfo.php
@@ -7,8 +7,8 @@
   *@link http://goma-cms.org
   *@license: http://www.gnu.org/licenses/gpl-3.0.html see "license.txt"
   *@Copyright (C) 2009 - 2012  Goma-Team
-  * last modified: 23.11.2012
-  * $Version 3.6.4
+  * last modified: 16.12.2012
+  * $Version 3.6.5
 */
 
 defined("IN_GOMA") OR die("<!-- restricted access -->"); // silence is golden ;)
