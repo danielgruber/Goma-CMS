56875496cee3387be9d5e50acea3f622030a3019
DanielGruber <daniel@ibpg.eu>
2012-12-16 03:04:30 +0100

    Improved editor.css handling

--- system/form/HTMLEditor.php
+++ system/form/HTMLEditor.php
@@ -4,8 +4,8 @@
   *@link http://goma-cms.org
   *@license: http://www.gnu.org/licenses/gpl-3.0.html see 'license.txt'
   *@Copyright (C) 2009 - 2011  Goma-Team
-  * last modified: 01.12.2011
-  * $Version 1.2.4
+  * last modified: 15.12.2011
+  * $Version 1.2.5
 */
 
 defined('IN_GOMA') OR die('<!-- restricted access -->'); // silence is golden ;)
@@ -155,10 +155,10 @@ window.toggleEditor_'.$this->input->id.' = function() {
 			$css = self::importCSS("tpl/" . Core::getTheme() . "/editor.css");
 			
 			// parse CSS
-			$css = preg_replace_callback('/([\.a-zA-Z0-9_\-,#\>\s\:\[\]\=]+)\s*{/Usi', array("historyController", "interpretCSS"), $css);
+			//$css = preg_replace_callback('/([\.a-zA-Z0-9_\-,#\>\s\:\[\]\=]+)\s*{/Usi', array("historyController", "interpretCSS"), $css);
 			FileSystem::write($cache, $css);
 			
-			return $cache;
+			return BASE_URI . $cache;
 		} else {
 			return false;
 		}
