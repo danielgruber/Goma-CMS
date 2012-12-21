9fb9df7faaa11d7c951df142ebc4347237224f3e
DanielGruber <daniel@ibpg.eu>
2012-12-21 01:22:09 +0100

    Version Update

--- gomacms/application/model/pages.php
+++ gomacms/application/model/pages.php
@@ -4,8 +4,8 @@
   *@link http://goma-cms.org
   *@license: http://www.gnu.org/licenses/gpl-3.0.html see 'license.txt'
   *@Copyright (C) 2009 - 2012  Goma-Team
-  * last modified: 19.12.2012
-  * $Version 2.4.6
+  * last modified: 21.12.2012
+  * $Version 2.4.7
 */
 
 defined('IN_GOMA') OR die('<!-- restricted access -->'); // silence is golden ;)
