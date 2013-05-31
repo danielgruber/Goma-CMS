3dd38900577858d181d9aa98a1c8ce2024947bd4
Daniel Gruber <daniel@ibpg.eu>
2013-05-31 03:42:38 +0200

    Title-Fields are now dynamic of tab

--- gomacms/application/model/pages.php
+++ gomacms/application/model/pages.php
@@ -711,22 +711,12 @@ class Pages extends DataObject implements PermProvider, HistoryData, Notifier
 				
 				$form->add(new TabSet('tabs', array(
 						new Tab('content', array(
-							$title = new textField('title', lang("title_page", "title of the page")),
-							$mainbartitle = new textField('mainbartitle', lang("menupoint_title", "title on menu")),
 							
-							$parenttype = new ObjectRadioButton("parenttype", lang("hierarchy", "hierarchy"), array(
-								"root" => lang("no_parentpage", "Root Page"),
-								"subpage" => array(
-									lang("subpage","sub page"),
-									"parent"
-								)
-							)),
-							$parentDropdown = new HasOneDropDown("parent", lang("parentpage", "Parent Page"), "title", ' `pages`.`class_name` IN ("'.implode($allowed_parents, '","').'") AND `pages`.`id` != "'.$this->id.'"'),
 							
 						), lang("content", "content")),
 						
 						new Tab('meta', array(
-							
+														
 							$description = new textField('meta_description', lang("site_description", "Description of this site")),
 							$wtitle = new TextField("googletitle", lang("window_title")),
 							new checkbox('mainbar', lang("menupoint_add", "Show in menus")),
@@ -743,6 +733,29 @@ class Pages extends DataObject implements PermProvider, HistoryData, Notifier
 					) 
 				));
 				
+				// append the field title, mainbar-title and hierarchy
+				// they are in Editor-Area in an other tab than in add-view
+				if($this->id == 0) {
+					$titleHolder = $form->content;
+				} else {
+					$titleHolder = $form->meta;
+				}
+				
+				$titleHolder->add($title = new textField('title', lang("title_page", "title of the page")), 0);
+				$titleHolder->add($mainbartitle = new textField('mainbartitle', lang("menupoint_title", "title on menu")), 0);
+				
+				$titleHolder->add($parentDropdown = new HasOneDropDown("parent", lang("parentpage", "Parent Page"), "title", ' `pages`.`class_name` IN ("'.implode($allowed_parents, '","').'") AND `pages`.`id` != "'.$this->id.'"'), 1);
+				
+				$titleHolder->add($parenttype = new ObjectRadioButton("parenttype", lang("hierarchy", "hierarchy"), array(
+					"root" => lang("no_parentpage", "Root Page"),
+					"subpage" => array(
+						lang("subpage","sub page"),
+						"parent"
+					)
+				)), 1);
+				
+				
+				// check for permissions
 				if(!$this->can("Write") || !Permission::check("PAGES_WRITE")) {
 					$write->disable();
 				}
