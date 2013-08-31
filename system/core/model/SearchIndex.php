93d306cb429c0ee9e60232b9ab44a9ceaf3ab0bf
Daniel Gruber <igruber@me.com>
2013-08-31 15:39:42 +0200

    Begin of good Search

---
+++ system/core/model/SearchIndex.php
@@ -0,0 +1,16 @@
+<?php defined('IN_GOMA') OR die();
+/**
+ * @package 	goma framework
+ * @link 		http://goma-cms.org
+ * @license 	LGPL http://www.gnu.org/copyleft/lesser.html see 'license.txt'
+*/
+
+/**
+ * every class which should search with relevance should extend this controller.
+ *
+ * @author 	Goma-Team
+ * @version 	1.0
+*/
+class SearchIndex extends DataObjectExtension {
+	
+}
