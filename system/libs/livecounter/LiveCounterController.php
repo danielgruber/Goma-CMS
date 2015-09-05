88fd34fc7cbd0a6ca28b236f9664ab97dd470be6
Daniel Gruber <daniel@ibpg.eu>
2015-09-05 13:50:32 +0200

    Refactoring in Statistics Implementation

---
+++ system/libs/livecounter/LiveCounterController.php
@@ -0,0 +1,29 @@
+<?php defined("IN_GOMA") OR die();
+
+/**
+ * Stat Migration.
+ *
+ * @author		Goma-Team
+ * @license		GNU Lesser General Public License, version 3; see "LICENSE.txt"
+ * @package		Goma\Framework
+ * @version		1.1.1
+ */
+class livecounterController extends Controller {
+    /**
+     * allowed actions.
+     */
+    public $allowed_actions = array(
+        "migrateStats"
+    );
+
+    public function migrateStats() {
+        if(!Core::is_ajax()) {
+            exit;
+        }
+
+        Core::globalSession()->stopSession();
+        ignore_user_abort(true);
+
+        livecounter::migrateStats();
+    }
+}
