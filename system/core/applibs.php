1cae6d685396c9f762b6620eedbbac72261f4a2c
Daniel Gruber <daniel@ibpg.eu>
2015-11-04 23:43:23 +0100

    added guid function

--- system/core/applibs.php
+++ system/core/applibs.php
@@ -889,6 +889,16 @@ function writeServerConfig() {
 	}
 }
 
+function GUID()
+{
+	if (function_exists('com_create_guid') === true)
+	{
+		return trim(com_create_guid(), '{}');
+	}
+
+	return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535));
+}
+
 /**
  * returns all http-headers.
  */
